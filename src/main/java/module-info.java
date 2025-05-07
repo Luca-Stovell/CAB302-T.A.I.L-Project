adf8667def737c3c428ddcf8298ceaa0d9e6be4b
Zenoor Baig <zenoorbaig@hotmail.co.uk>
2025-05-07 21:19:45 +1000

    Added functionality to student ai assist

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,19 @@
 module com.example.cab302tailproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.desktop;
     requires java.sql;
-
     requires java.net.http;
     requires ollama4j;
 
-    opens com.example.cab302tailproject to javafx.fxml;
+    opens com.example.cab302tailproject to javafx.fxml, javafx.graphics;
     exports com.example.cab302tailproject;
-    exports com.example.cab302tailproject.controller;
+
     opens com.example.cab302tailproject.controller to javafx.fxml;
-    exports com.example.cab302tailproject.model;
-    opens com.example.cab302tailproject.model to javafx.fxml;
-    exports com.example.cab302tailproject.ollama4j;
-    opens com.example.cab302tailproject.ollama4j to javafx.fxml;
-    exports com.example.cab302tailproject.controller.teachercontroller;
     opens com.example.cab302tailproject.controller.teachercontroller to javafx.fxml;
+    opens com.example.cab302tailproject.controller.studentcontroller to javafx.fxml;
+
+    exports com.example.cab302tailproject.model;
+    exports com.example.cab302tailproject.ollama4j;
 }
